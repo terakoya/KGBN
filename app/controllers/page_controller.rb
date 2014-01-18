cfde2a181c636ef9f41f354f087eac5621657870
Kazuto Kawamura <k.kazooto@gmail.com>
2014-01-18 15:01:31 +0900

    ログイン機能の修正
    ユーザーがログインしていない場合、サインインページへ飛ばすように
    各コントローラーにbefore_filterを追加

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,8 +1,5 @@
 class PageController < ApplicationController
   def index
-    @topics = Topic.all
-    if !current_user
-      redirect_to new_user_session_path
-    end
+    @topics = Topic.all 
   end
 end
